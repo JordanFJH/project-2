7178b5c167e4095a546c875141e793234509a590
Jordan <higgsfj@gmail.com>
2024-01-04 21:05:10 -0500

    setup to test the drag feature for html

--- src/pages/SynMatchPage.jsx
+++ src/pages/SynMatchPage.jsx
@@ -3,7 +3,11 @@
 function SynMatchPage(props) {
     return (
         <div>
-            <h1>This is the page for SynMatch!!!</h1>
+            <h1>This is the page for SynMatch!!! Time to do some testing</h1>
+            <div className="test-holder">
+                <div><h3>Drag me</h3></div>
+                <div></div>
+            </div>
         </div>
     );
 }
