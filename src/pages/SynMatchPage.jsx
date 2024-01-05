8c9edb736cc4bb5ab2522f8c4a826c5f2fce4a88
Jordan <higgsfj@gmail.com>
2024-01-05 09:41:49 -0500

    removed all drag and drop attempts

--- src/pages/SynMatchPage.jsx
+++ src/pages/SynMatchPage.jsx
@@ -1,13 +1,14 @@
 
 
 function SynMatchPage(props) {
+
+    
+
+
     return (
         <div>
             <h1>This is the page for SynMatch!!! Time to do some testing</h1>
-            <div className="test-holder">
-                <div><h3>Drag me</h3></div>
-                <div></div>
-            </div>
+            
         </div>
     );
 }
